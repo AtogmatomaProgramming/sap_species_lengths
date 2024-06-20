14c74591b6c121bce0575e73860c210ee1e6cadd
Alberto Candelario Brito <alberto.candelario@st.ieo.es>
2024-06-20 14:16:07 +0200

    formal version 1.0 of the script

--- functions.R
+++ functions.R
@@ -1,68 +1,141 @@
+#' Principal functions
+
+#' @name draw_interactive_boxplot
+#' @description Function to draw interactive box plots for
+#' the distribution of the lengths for the diverse
+#' fishing grounds.
+#' @param measure_ranges: dataframe with the different measures
+#' for the species.
+#' @param fishing_ground: a unitary string vector with the acronym
+#' of the fishing ground
+#' @param result_directory: a string value with the name of the
+#' directory where the interactive graphics will be stored
+#' @return the interactive boxplot for species according to
+#' fishing ground
+
+draw_interactive_boxplot <- function(measure_ranges, fishing_ground, result_directory) {
+  measure_ranges <- manipulate_measure_range(measure_ranges, fishing_ground)
+
+  plot <- ggplot2::ggplot(data = measure_ranges, aes(x = ESP_CAT, y = TALLA)) +
+    ggplot2::stat_boxplot(geom = "errorbar", width = 0.5) +
+    ggplot2::geom_boxplot(fill = "#4271AE", outlier.colour = "red", alpha = 0.9) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1)) +
+    ggplot2::ggtitle(paste0("Distribución tallas caladero ", fishing_ground))
+
+  p <- plotly::ggplotly(plot)
+
+  htmlwidgets::saveWidget(
+    p,
+    paste0(
+      getwd(),
+      "/",
+      result_directory,
+      "/grafico_interactivo_tallas_",
+      fishing_ground,
+      ".html"
+    )
+  )
+}
+
+#' @name filter_measure_dataframe
+#' @description function to get the stat values for
+#' the lengths of the species according to the fishing grounds.
+#' @param measure_ranges: dataframe with the different measures
+#' for the species.
+#' @param fishing_ground: a unitary string vector with the acronym
+#' of the fishing ground
+#' @param result_directory: a string value with the name of the
+#' directory where the interactive graphics will be stored
+#' @return a dataset with the stat values for
+#' the lengths of the species according to the fishing grounds.
+
+filter_measure_dataframe <- function(measure_ranges, fishing_ground) {
+  measure_ranges <- manipulate_measure_range(measure_ranges, fishing_ground)
+
+  # Step 2: Make the boxplot from the measures
+  m_graphic <- boxplot(TALLA ~ ESP_CAT, data = measure_ranges)
+
+  # Step 3: Take the dataframe with the stats
+  m_stats <- as.data.frame(t(m_graphic$stats))
+
+  # Step 4: modify the m_stats dataframe
+  m_stats$Species <- m_graphic$names
+
+  columns_names <- c("Down_Whisker", "First_Quartile", "Median", "Third_Quartile", "Upper_Whisker", "Species")
+
+  colnames(m_stats) <- columns_names
+
+  m_stats <- m_stats[, c("Species", "Down_Whisker", "Upper_Whisker")]
+
+  m_stats <- m_stats[!is.na(m_stats$Down_Whisker), ]
+
+  m_stats$CALADERO <- fishing_ground
+
+  return(m_stats)
+}
+
+#' @name lengths_and_graphics
+#' @description Function to obtain the final dataset and the
+#' interactive boxplot for all the species from the working
+#' fishing grounds
+#' @param measure_ranges: dataframe with the different measures
+#' for the species.
+#' @param fishing_environment: dataframe with the name, acronym and
+#' code of the fishing grounds
+#' @param result_directory: a string value with the name of the
+#' directory where the interactive graphics will be stored
+#' @return The final dataset and the
+#' interactive boxplot for all the species from the working
+#' fishing grounds
 
 
-#Functions
+lengths_and_graphics <- function(measure_ranges, fishing_environment, result_directory) {
+  measure_ranges <- merge(measure_ranges, 
+                          fishing_environment[, c("CALADERO", "COD_ORIGEN")], 
+                          by = "COD_ORIGEN", 
+                          all.x = TRUE)
 
-take_range_measures <- function(measure_ranges, fishing_environment, result_directory){
-  
-  #Step 1: Add the fishing environment to our measure_ranges
-  measure_ranges <- merge(measure_ranges, fishing_environment[, c("CALADERO", "COD_ORIGEN")], by = "COD_ORIGEN", all.x = TRUE)
-  
-  #Step 2: take the vector with the value of the fishing grounds
-  
   fishing_grounds <- unique(measure_ranges$CALADERO)
-  
-  #Step 3: remove Na values from fishing_grounds
-  
-  fishing_grounds <- fishing_grounds[!is.na(fishing_grounds)]
-  
-  #Step 4: filter the measure_range according to the fishing_ground
-  #and take the columns "ESP_CAT" and "TALLA"
 
-  lista_outliers <- lapply(fishing_grounds, function(i){
+  fishing_grounds <- fishing_grounds[!is.na(fishing_grounds)]
+
+  lista_outliers <- lapply(fishing_grounds, function(i) {
     filter_measure_dataframe(measure_ranges, i)
   })
-  
-  #Step 5: export the dataframes from the fishing grounds and save them as csv file
-  
+
   df_outliers <- do.call(rbind.data.frame, lista_outliers)
-  
+
   df_outliers[, -c(1)]
-  
-  sapmuebase::exportCsvSAPMUEBASE(df_outliers, "outliers_especies_2019_2023.csv", path = paste0(getwd(),"/",result_directory))
-  
+
+  sapmuebase::exportCsvSAPMUEBASE(df_outliers, "outliers_especies_2019_2023.csv", path = paste0(getwd(), "/", result_directory))
+
   df_outliers
-  
+
+  lapply(fishing_grounds, function(i) {
+    draw_interactive_boxplot(measure_ranges, i, result_directory)
+  })
 }
 
 
-filter_measure_dataframe <- function(measure_ranges, fishing_ground){
-  
-  #Step 1.1: transform unitary vector to a string
+#' @name manipulate_measure_range
+#' @description function to filter the measure_ranges dataframe
+#' according to fishing ground, deleting the NA values and taking
+#' only the columns "ESP_CAT" and "TALLA"
+#' @param measure_ranges: dataframe with the different measures
+#' for the species.
+#' @param fishing_ground: a unitary string vector with the acronym
+#' of the fishing ground
+#' @return the measure_ranges dataframe according to fishing ground,
+#' deleting the NA values and taking
+#' only the columns "ESP_CAT" and "TALLA"
+
+
+manipulate_measure_range <- function(measure_ranges, fishing_ground) {
   fishing_ground <- as.character(fishing_ground)
-  
-  measure_ranges <- measure_ranges[measure_ranges$CALADERO==fishing_ground, c("ESP_CAT", "TALLA")]
-  
-  #Step 2: Make the boxplot from the measures
-  m_graphic <- boxplot(TALLA ~ ESP_CAT, data = measure_ranges)
-  
-  #Step 3: Take the dataframe with the stats
-  m_stats <- as.data.frame(t(m_graphic$stats))
-  
-  #Step 4: modify the m_stats dataframe 
-  m_stats$Species <- m_graphic$names
-  
-  columns_names <- c("Down_Whisker", "First_Quartile", "Median", "Third_Quartile", "Upper_Whisker", "Species")
-  
-  colnames(m_stats) <- columns_names
-  
-  m_stats <- m_stats[, c("Species", "Down_Whisker", "Upper_Whisker")]
-  
-  m_stats <- m_stats[!is.na(m_stats$Down_Whisker), ]
-  
-  m_stats$CALADERO <- fishing_ground
-  
-  return(m_stats)
-  
+
+  measure_ranges <- measure_ranges[measure_ranges$CALADERO == fishing_ground, c("ESP_CAT", "TALLA")]
+
+  measure_ranges <- measure_ranges[!is.na(measure_ranges$TALLA), ]
+
+  return(measure_ranges)
 }
-
-
--- principal_script.R
+++ principal_script.R
@@ -1,4 +1,6 @@
-#Step 1: Import the data sets from the folder "data"
+#Import libraries
+
+library(ggplot2)
 
 #Define the data directory source
 
@@ -6,10 +8,14 @@ data_source <- paste0(getwd(), "/data/")
 
 #Import the work files
 
-measure_ranges <- readRDS(paste0(data_source,"rim_lengths_2019_2023.rds"))
+measure_ranges <- readRDS(paste0(data_source,
+                                 "rim_lengths_2019_2023.rds"))
 
-fishing_environment <- read.table(file = paste0(data_source,"caladero.csv"), head = TRUE, sep = ";",
-                                  fill = TRUE, colClasses = c("character", "factor", "character"))
+fishing_environment <- read.table(file = paste0(data_source,"caladero.csv"), 
+                                  head = TRUE, 
+                                  sep = ";",
+                                  fill = TRUE, 
+                                  colClasses = c("character", "factor", "character"))
 
 #Import functions
 
@@ -17,4 +23,4 @@ source('functions.R')
 
 #Apply the function to obtain the dataframe with values of the outliers
 
-a <- take_range_measures(measure_ranges, fishing_environment, "results")
+a <- lengths_and_graphics(measure_ranges, fishing_environment, "results")
